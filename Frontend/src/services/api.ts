87345d37225bdf3e0b8ab3245e3be98dc43e9be9
arturguedees <rollembergartur@gmail.com>
2026-07-23 14:41:53 -0300

    feat: atualizado baseURL para a API em produção no Render

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// URL do back-end em .NET
+// URL do back-end em .NET (URL do Render que é o serviço de hospedagem que estou usando para o deploy do projeto)
 export const api = axios.create({
-  baseURL: 'http://localhost:5057', 
+  baseURL: 'https://sistemadegastos-fullstack.onrender.com', 
 });
